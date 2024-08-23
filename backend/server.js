a4b1de01408d95170fc7971851948ca9afa439c1
Ravi Ranjan Singh <ravi@trikara.com>
2024-08-23 17:46:37 +0000

    changes

--- backend/server.js
+++ backend/server.js
@@ -49,8 +49,7 @@ app.use(express.json());
 
 app.use(limiter)
 
-/*
-app.use(cors({
+/*app.use(cors({
   origin: 'https://appnew.apisecurityengine.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
